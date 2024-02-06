b2696a35b9462c58830d242403b718265d80e92c
Joe Andrews <joeandrews92@gmail.com>
2024-02-05 17:41:05 -0800

    create pages and setup routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import ErrorPage from "./error-page";
+import Shop from "./routes/shop";
+import Contact from "./routes/contact";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/root.jsx
@@ -0,0 +1,25 @@
+import {Outlet, Link } from "react-router-dom"
+
+export default function Root() {
+    return (
+        <>
+            <h1>My Shop</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={`/`}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={`shop`}>Shop</Link>
+                    </li>
+                    <li>
+                        <Link to={`contact`}>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div id="content">
+                <Outlet />
+            </div>
+        </>
+    )
+}
